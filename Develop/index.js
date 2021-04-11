0663988d7b7f8cae73d6b60222953579838f964e
Mark Raudenbush <markraud@hotmail.com>
2021-04-10 18:06:42 -0600

    set up the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,58 @@
 // TODO: Include packages needed for this application
+// I don't understand what it wants us to do here.  we need to have 
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of the application?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Please enter a description of the application.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'Please enter the installation instructions.',
+    name: 'install',
+  },
+  {
+    type: 'input',
+    message: 'Please enter the usage information.',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'Please choose a licence:',
+    name: 'license',
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+
+
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+.prompt(questions)
+.then((response) => {
+  console.log(response.title)
+  console.log(response.description)
+  console.log(response.install)
+  console.log(response.usage)
+  console.log(response.license) , (err) =>
+err ? console.error(err) : console.log('Success!'))
+);
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {
 
 // Function call to initialize app
-init();
+// init();
